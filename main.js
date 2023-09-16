6ae6c82322541e1642b815bf4185beb82625a1ba
izzatmammadov <imamedov388@gmail.com>
2023-09-16 21:49:40 +0400

    update

--- main.js
+++ main.js
@@ -1,31 +1,45 @@
-const inputBox = document.getElementById("input-box");
-const list = document.getElementById("list");
-const message = document.getElementById("message");
-
+const inputBox = document.querySelector("#input-box");
+const list = document.querySelector("#list");
+const message = document.querySelector("#message");
+const clearBtn = document.querySelector(".clear-btn");
 
 function addTask() {
-    if (inputBox.value === "") {
-        message.style.display="block"
-        setTimeout(() => {
-        message.style.display="none"
-        }, 1500);
-    }
-    else {
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        list.appendChild(li);
-        inputBox.value="";
-        let span = document.createElement("span");
-        span.innerHTML = "&times";
-        li.appendChild(span)
-    }
-};
+  if (inputBox.value === "") {
+    message.style.display = "block";
+    setTimeout(() => {
+      message.style.display = "none";
+    }, 1500);
+  } else {
+    clearBtn.style.display = "flex";
+    let li = document.createElement("li");
+    li.innerHTML =
+      inputBox.value.charAt(0).toUpperCase() + inputBox.value.slice(1);
+    list.appendChild(li);
+    inputBox.value = "";
+    let span = document.createElement("span");
+    span.innerHTML = "&times";
+    li.appendChild(span);
+  }
+}
 
-list.addEventListener("click", function(e) {
-    if(e.target.tagName === "LI") {
-        e.target.classList.toggle("checked")
-    }
-    else if (e.target.tagName === "SPAN") {
-        e.target.parentElement.remove()
-    }
-})
+inputBox.addEventListener("keydown", function (e) {
+  if (e.key == "Enter") {
+    addTask();
+  }
+});
+
+list.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+  } else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+  }
+});
+
+clearBtn.addEventListener("click", function () {
+  const liElements = list.querySelectorAll("li");
+  liElements.forEach((li) => {
+    li.remove();
+    clearBtn.style.display = "none";
+  });
+});
